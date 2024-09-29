9e9fb8fcd2bf2ee6232b4fa79934b7a1ac17b6da
noah brite <noah@Noahs-MacBook-Air.local>
2024-09-29 22:40:45 +0900

    ライブナビの表示を作った

---
+++ src/features/Livenav.tsx
@@ -0,0 +1,11 @@
+function Livenav(){
+    return(
+        <body className="bg-yellow-300">
+        <div className="text-2xl mx-5 my-5">
+          今行われているライブ
+        </div>
+      </body>
+    );
+}
+
+export default Livenav; 
