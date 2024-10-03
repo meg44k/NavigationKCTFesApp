0a3fd13e3105d8426e2136775b4f1c21c1fbaf09
kitani-y <nativetofu@gmail.com>
2024-10-03 22:54:19 +0900

    3～5年生のクラスリストを修正しました

--- src/features/Classlist.tsx
+++ src/features/Classlist.tsx
@@ -15,12 +15,13 @@ export default function ClassList({ grade }: ClassListProps) {
   const [selectedOption, setSelectedOption] = useState<Option | null>(null);
   const [isArrowActive, setArrowActive] = useState(false);
 
+  // gradeに応じて、1年と2年は「〇組」、3年から5年は「〇コース」と表示
   const options: Option[] = [
-    { value: 'option1', label: `${grade}年1組` },
-    { value: 'option2', label: `${grade}年2組` },
-    { value: 'option3', label: `${grade}年3組` },
-    { value: 'option4', label: `${grade}年4組` },
-    { value: 'option5', label: `${grade}年5組` },
+    { value: 'option1', label: grade <= 2 ? `${grade}年1組` : `${grade}年機械創造システムコース` },
+    { value: 'option2', label: grade <= 2 ? `${grade}年2組` : `${grade}年知能ロボットシステムコース` },
+    { value: 'option3', label: grade <= 2 ? `${grade}年3組` : `${grade}年電気電子コース` },
+    { value: 'option4', label: grade <= 2 ? `${grade}年4組` : `${grade}年情報システムコース` },
+    { value: 'option5', label: grade <= 2 ? `${grade}年5組` : `${grade}年物質科学コース` },
   ];
 
   const handleSelectOption = (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -34,25 +35,24 @@ export default function ClassList({ grade }: ClassListProps) {
   return (
     <main>
       <div className="container">
-      <div className="select-container">
-        <select
-          value={selectedOption?.value || ''}
-          onChange={handleSelectOption}
-          onFocus={() => setArrowActive(true)}
-          onBlur={() => setArrowActive(false)}
-        >
-          <option value="">{grade}年生</option>
-          {options.map((option) => (
-            <option key={option.value} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </select>
-        <div className={arrowClassName}></div>
+        <div className="select-container">
+          <select
+            value={selectedOption?.value || ''}
+            onChange={handleSelectOption}
+            onFocus={() => setArrowActive(true)}
+            onBlur={() => setArrowActive(false)}
+          >
+            <option value="">{grade}年生</option>
+            {options.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+          <div className={arrowClassName}></div>
+        </div>
+        {selectedOption && <p>{selectedOption.label}</p>}
       </div>
-      {selectedOption && <p>{selectedOption.label}</p>}
-    </div>
     </main>
   );
 }
-
