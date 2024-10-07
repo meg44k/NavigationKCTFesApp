9aee1b09a418575eeb2b6ccabd1a16c6d592970f
noah brite <noah@Noahs-MacBook-Air.local>
2024-10-07 21:42:19 +0900

    ライブディティールの見た目

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
 // import Image from "next/image";
 // import Livenav from "@/features/Livenav";
+import Teams from "@/features/Teams";
 import {useState} from 'react';
+import Image from 'next/image';
 
 const now = new Date();
 const hour = now.getHours();
 const minute = now.getMinutes();
 
+const team1 = {
+  name : '豚汁’s',
+  member : ['木谷佑磨','藤本陽向','占部千馬','冨崎和音']
+}
+
+
 export function Schedule(props) {
   return (
     <div className="bg-white text-xl rounded-2xl mx-7 mb-5 p-2">
-      <div className="mx-7">{props.time}</div>
+      <div className="mx-2">{props.time}</div>
       <div className="text-center">{props.name}</div>     
     </div>
   );
@@ -18,9 +26,9 @@ export function Schedule(props) {
 export function LiveDetail(props) {
   return(
     <div className="bg-yellow-300 border border-black mx-10 rounded-2xl">
-      <ul className="flex border-b border-black ml-5 mr-5">
-        <li className="py-2 pr-20 text-xl">{props.name}</li>
-        <li className="text-xl"></li>
+      <ul className="flex border-b border-black justify-between mx-5">
+        <li className="py-2 text-xl">{props.team.name}</li>
+        <li className="w-6 h-6 pt-3"><img src="/BlackCross.svg" alt="Icon"/></li>
       </ul>
       <div className="py-2 pl-5 text-xl">バンドメンバー</div>
       <div className="py-2 pl-5 text-xl">コメント</div>
@@ -32,7 +40,7 @@ export default function Home() {
   return (
       <div className="bg-yellow-400 min-h-screen">
         <div className="pt-40"> 
-          <LiveDetail name='豚汁’s'/>
+          <LiveDetail team={team1}/>
         </div>
         <div className="text-2xl px-5 py-5"> 
           今行われているライブ
---
+++ src/features/Teams.tsx
@@ -0,0 +1,7 @@
+const team1 = {
+    name : '豚汁’s',
+    member : ['木谷佑磨','藤本陽向','占部千馬','冨崎和音']
+    comment : '似たキーマ'
+}
+
+export default Teams;
