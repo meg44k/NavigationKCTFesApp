1fd3a96539bdb1fd909dc9ce367b4afc0ed59fe1
kitani-y <nativetofu@gmail.com>
2024-10-09 00:03:55 +0900

    UIを修正しました

--- src/features/Classlist.tsx
+++ src/features/Classlist.tsx
@@ -15,13 +15,12 @@ export default function ClassList({ grade }: ClassListProps) {
   const [selectedOption, setSelectedOption] = useState<Option | null>(null);
   const [isArrowActive, setArrowActive] = useState(false);
 
-  // gradeに応じて、1年と2年は「〇組」、3年から5年は「〇コース」と表示
   const options: Option[] = [
     { value: 'option1', label: grade <= 2 ? `${grade}年1組` : `${grade}年機械創造システムコース` },
     { value: 'option2', label: grade <= 2 ? `${grade}年2組` : `${grade}年知能ロボットシステムコース` },
     { value: 'option3', label: grade <= 2 ? `${grade}年3組` : `${grade}年電気電子コース` },
     { value: 'option4', label: grade <= 2 ? `${grade}年4組` : `${grade}年情報システムコース` },
-    { value: 'option5', label: grade <= 2 ? `${grade}年5組` : `${grade}年物質科学コース` },
+    { value: 'option5', label: grade <= 2 ? `${grade}年5組` : `${grade}年物質化学コース` },
   ];
 
   const handleSelectOption = (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -33,26 +32,27 @@ export default function ClassList({ grade }: ClassListProps) {
   const arrowClassName = `select-arrow ${isArrowActive ? 'active' : ''}`;
 
   return (
-    <main>
+    <>
       <div className="container">
-        <div className="select-container">
+        <div className="select-container w-full flex justify-center">
           <select
             value={selectedOption?.value || ''}
             onChange={handleSelectOption}
             onFocus={() => setArrowActive(true)}
             onBlur={() => setArrowActive(false)}
+            className="w-3/5 p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
           >
-            <option value="">{grade}年生</option>
+            <option value="" className="w-3/5">{grade}年生</option>
             {options.map((option) => (
-              <option key={option.value} value={option.value}>
+              <option key={option.value} value={option.value} className="w-3/5">
                 {option.label}
               </option>
             ))}
           </select>
           <div className={arrowClassName}></div>
         </div>
-        {selectedOption && <p>{selectedOption.label}</p>}
+        {/* {selectedOption && <p className="mt-4 text-lg">{selectedOption.label}</p>} */}
       </div>
-    </main>
+    </>
   );
 }
